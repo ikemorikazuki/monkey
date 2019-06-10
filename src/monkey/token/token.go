dc8fd921de7f0422e1336687298371227459f5ec
ikemorikazuki <kazuonsunday.yahoo.co,jp>
2019-06-10 20:33:36 +0900

    lexer test_lexer token

--- src/monkey/token/token.go
+++ src/monkey/token/token.go
@@ -1,1 +1,35 @@
+// token/token.go
+package token
 
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILEGAL = "ILEGAL"
+	EOF    = "EOF"
+
+	// 識別子 + リテラル
+	IDENT = "IDENT" // add, foobar, x, y,
+	INT   = "INT"   // 123456
+
+	// 演算子
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// デリミタ
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// キーワード
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
