ca643d192a4c5004bdab28ec24cdb05d47e62dd0
ikemorikazuki <kazuonsunday.yahoo.co,jp>
2019-08-15 22:58:04 +0900

    change directry

---
+++ ast/ast.go
@@ -0,0 +1,48 @@
+// ast/ast.go
+
+package ast
+
+import "token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // token.LET トークン 現在のノード
+	Name  *Identifier // 接続している エッジ
+	Value Expression  // 接続している エッジ
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // token.IDENT トークン
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
