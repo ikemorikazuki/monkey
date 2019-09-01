afdda2d240866a910be525f0006d9ff0902a21a9
ikemorikazuki <kazuonsunday.yahoo.co,jp>
2019-09-01 15:17:34 +0900

    change if statement UI

--- src/monkey/ast/ast.go
+++ src/monkey/ast/ast.go
@@ -183,14 +183,18 @@ func (ie *IfExpression) expressionNode()      {}
 func (ie *IfExpression) TokenLiteral() string { return ie.Token.Literal }
 func (ie *IfExpression) String() string {
 	var out bytes.Buffer
-	out.WriteString("if")
+	out.WriteString("if ")
 	out.WriteString(ie.Condition.String())
 	out.WriteString(" ")
+	out.WriteString("{ ")
 	out.WriteString(ie.Consequence.String())
+	out.WriteString(" }")
 
 	if ie.Alternative != nil {
-		out.WriteString("else ")
+		out.WriteString(" else ")
+		out.WriteString("{ ")
 		out.WriteString(ie.Alternative.String())
+		out.WriteString(" }")
 	}
 	return out.String()
 }
