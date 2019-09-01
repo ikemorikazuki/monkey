ab0e9a03ce4815fc52c77fc1e4b5bda36eead2da
ikemorikazuki <kazuonsunday.yahoo.co,jp>
2019-09-01 14:17:57 +0900

    finish 4.2.2

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -58,6 +58,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
 	p.registerPrefix(token.INT, p.parseIntegerLiteral)
+	p.registerPrefix(token.STRING, p.parseStringLiteral)
 	p.registerPrefix(token.BANG, p.parsePrefixExpression)
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	p.registerPrefix(token.TRUE, p.parseBoolean)
@@ -427,4 +428,8 @@ func (p *Parser) curPrecdecnce() int {
 	}
 
 	return LOWEST
+}
+
+func (p *Parser) parseStringLiteral() ast.Expression {
+	return &ast.StringLiteral{Token: p.curToken, Value: p.curToken.Literal}
 }
